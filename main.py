93bf3cffb9731aac814cf78cfb2da37ea362badf
hikaru_kl <maximfilonenko2@gmail.com>
2024-05-13 22:32:18 +0700

    Answers output feature
    Update data.zip (new tasks and answers)
    Now, for answer generation, you need to save a .txt file in the directory where you store your task image and rename this file to "task_image_name.txt". In this file, you need to write the answer on the first line.

--- main.py
+++ main.py
@@ -11,7 +11,9 @@ import PIL.Image
 from urllib.request import urlretrieve
 from reportlab.pdfgen import canvas
 from reportlab.lib.utils import ImageReader
-
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+pdfmetrics.registerFont(TTFont('Arial', 'arial.ttf'))
 
 ROOT_DIR = os.path.abspath('.')
 
@@ -22,43 +24,54 @@ logger.addHandler(logging.StreamHandler())
 def main(config: dict) -> None:
     tasks = data_loader(config)
     Y_MARGIN = config['y_margin_px']
-    filename = f'variant_{str(uuid.uuid4())[:6]}.pdf'
+    id_ = str(uuid.uuid4())[:6]
+    filename = f'variant_{id_}.pdf'
     A4 = (1240, 1754)
     c = canvas.Canvas(filename, pagesize=A4)
+    c.setFont('Arial', 28)
     page_width, page_height = A4
 
     current_image_y = 0
-    for i in range(1, len(tasks.keys())):
+
+    answers_file = open(f'./answers_{id_}.txt', 'w', encoding='utf-8')
+    for i in tasks.keys():
 
         # Scaling image
-        image = PIL.Image.open(tasks[i])
+        image = PIL.Image.open(tasks[i][0])
 
         if image.width > page_width - 10:
             if config["debug"]:
                 logger.info(
-                    f'Image {tasks[i]} has been resized (image.width > page_width)')
+                    f'Image {tasks[i][0]} has been resized (image.width > page_width)')
             new_height = math.ceil(
                 image.height * ((page_width - 10) / image.width))
             image = image.resize((page_width - 10, new_height))
         elif image.width < (page_width - 10) * 0.9:
             if config["debug"]:
                 logger.info(
-                    f'Image {tasks[i]} has been resized (image.width < page_width)')
+                    f'Image {tasks[i][0]} has been resized (image.width < page_width)')
             new_height = math.ceil(
                 image.height * ((page_width - 10) / image.width))
             image = image.resize((page_width - 10, new_height))
         reportlab_pil_img = ImageReader(image)
         if i == 1:
-            current_image_y = page_height - image.height - 10
+            current_image_y = page_height - image.height - 40
+            c.drawString(x=10, y=page_height - 28, text=f'Задание №{i}')
         else:
             if current_image_y - image.height - Y_MARGIN >= Y_MARGIN:
                 current_image_y -= image.height + Y_MARGIN
             else:
                 c.showPage()
+                c.setFont('Arial', 28)
                 current_image_y = page_height - image.height - Y_MARGIN
+            c.drawString(x=10, y=current_image_y +
+                         image.height, text=f'Задание №{i}')
         c.drawImage(reportlab_pil_img, x=(
-            page_width - reportlab_pil_img.getSize()[0]) / 2, y=current_image_y)
+            page_width - reportlab_pil_img.getSize()[0]) / 2, y=current_image_y-28)
+        answers_file.write(
+            f'{i}. {open(tasks[i][1], encoding="utf-8").readline()}\n' if tasks[i][1] else f'{i}. Ответ не найден\n')
     c.save()
+    answers_file.close()
     logger.info(f'Вариант {filename} готов!')
     os.system('pause')
 
@@ -67,12 +80,19 @@ def data_loader(config: dict) -> dict:
     tasks = {}.fromkeys(range(1, config['tasks_amount'] + 1), [])
     for i in range(1, config['tasks_amount'] + 1):
         try:
-            tasks[i] = os.path.abspath(
-                ROOT_DIR + f'/data/{i}/{random.choice(os.listdir(path=ROOT_DIR + f"/data/{i}/"))}')
+            while True:
+                files = os.listdir(
+                    path=ROOT_DIR + f"/data/{i}/")
+                task_path = random.choice(files)
+                if task_path.split('.')[-1] != 'txt':
+                    break
+            filename, file_extension = os.path.splitext(task_path)
+
+            tasks[i] = (os.path.abspath(
+                ROOT_DIR + f'/data/{i}/{task_path}'), os.path.abspath(ROOT_DIR + f'/data/{i}/{filename}.txt') if os.path.isfile(ROOT_DIR + f'/data/{i}/{filename}.txt') else None)
         except IndexError:
             logger.warning(f'Отсутствуют задания №{i}')
             del tasks[i]
-            # time.sleep(0.1)
     return tasks
 
 
